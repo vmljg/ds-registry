b01b0e87af2c261a6f5b47a196f6103af837a018
Jason Gardner <jason.gardner@vml.com>
2025-10-02 08:18:13 -0500

    upd: Provide unique TS config for Next.js build (#24)
    * upd: Provide unique TS config for Next.js build
    
    * chore: Apply formatting

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  typescript: {
+    tsconfigPath: "tsconfig.build.json",
+  },
   async headers() {
     return [
       {
